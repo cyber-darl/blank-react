06363824d46c57f1729910fb5fb5f0e77cdb5d05
cyber-darl <onosdarlington939@gmail.com>
2025-03-24 17:49:24 +0100

    switch

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const connectToMongo = require('./db');
-const path = require('path');
-
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 4001;
+
 
 // Middleware
 app.use(express.json());
@@ -13,20 +13,19 @@ app.use(cors());
 // Connect to MongoDB
 connectToMongo();
 
-// Serve the React build folder **correctly**
-app.use(express.static(path.join(__dirname, '../build')));
+// Routes
+app.use('/api/auth', require('./routes/auth'));
 
-// Serve React app for all other routes
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-// Health check route
 app.get('/', (req, res) => {
-  res.send('Hello from Express Server!');
+    res.send('Hello World!');
 });
 
-// Start the server
+  // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+console.log(`Server is running on port http://localhost:${PORT}`);
 });
