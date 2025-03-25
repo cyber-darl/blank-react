ca457e394fc9cf345be127b1190529a9514e0558
cyber-darl <onosdarlington939@gmail.com>
2025-03-25 09:58:28 +0100

    vercel-i

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World');
 });
 
   // Start the server
