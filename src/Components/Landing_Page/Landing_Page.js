2b9622a0b0e5cffe916697ced09a719950a3a479
cyber-darl <onosdarlington939@gmail.com>
2025-11-13 10:56:50 +0100

    mobile changes4

--- src/Components/Landing_Page/Landing_Page.js
+++ src/Components/Landing_Page/Landing_Page.js
@@ -1,6 +1,4 @@
 import React,{useEffect} from "react"; // Importing the necessary modules from React library
-import AOS from 'aos'; //Import script from animate on scroll css library
-import 'aos/dist/aos.css'; //Import styles from animate on scroll css library
 import { Link } from "react-router-dom"; // Importing the Link component from react-router-dom library
 import Navbar from '../Navbar/Navbar'; //Importing the navigation bar component
 import Notification from '../Notification/Notification';
@@ -8,9 +6,7 @@ import "./LandingPage.css"; // Importing the CSS styles for the Landing_Page com
 
 // Defining the Function component Landing_Page
 const Landing_Page = () => {
-  useEffect(() => {
-    AOS.init();
-  }, [])
+
 
   return (
     <section className="hero-section"> {/* Creating a section with class name 'hero-section' */}
@@ -44,5 +40,4 @@ const Landing_Page = () => {
     </section>
   );
 };
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 export default Landing_Page; // Exporting the Landing_Page component to be used in other parts of the application
